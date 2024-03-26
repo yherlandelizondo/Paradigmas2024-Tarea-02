320e003bc97a9e5cc5386979cdff5c4814be320c
Gaby283 <gaquesada@estudiantec.cr>
2024-03-26 15:45:47 -0600

    Ventana Selec

--- interfaz.rkt
+++ interfaz.rkt
@@ -5,24 +5,63 @@
 
 #lang racket
 (require racket/gui/base)
-
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;structure: Main Window;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define(holi)
+  (displayln "holi"))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;structure: Pantalla principal;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 
 (define mainWindow (new frame% [label "BlaCEJack"]
-                        [width 1100]
-                        [height 550]))
+                        [width 900]
+                        [height 700]))
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;structure: Window layout section;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-(define mainPanel (new horizontal-panel% [parent mainWindow]))
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;structure: Disposicion de la pantalla;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define mainPanel (new vertical-panel% [parent mainWindow])) ;panel principal
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;graphics: canvas section;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define upperPanel (new horizontal-panel% [parent mainPanel] ;panel superior 
+                       [min-width 900] 
+                       [min-height 280]                     
+                       ))
+
+(define lowerPanel (new horizontal-panel% [parent mainPanel] ;panel inferior
+                        [min-width 900]
+                        [min-height 420]
+                        ))
+
+(define player1Panel (new vertical-panel% [parent lowerPanel] ; panel para jugador 1
+                        [min-width 300]
+                        [min-height 420]
+                        ))
+
+(define player2Panel (new vertical-panel% [parent lowerPanel] ; panel para jugador 2
+                        [min-width 300]
+                        [min-height 420]
+                        ))
+
+(define player3Panel (new vertical-panel% [parent lowerPanel] ; panel para jugador 3
+                        [min-width 300]
+                        [min-height 420]
+                        ))                     
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;graphics: Lineas divisoras;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+(define (draw-lines canvas)
+  (define dc (send canvas get-dc))
+  (send dc set-pen "black" 2 'solid)
+  (send dc draw-line 0 280 900 280) ; División horizontal
+  
+  ; Dibujar líneas verticales en la sección de abajo
+  (send dc draw-line 300 280 300 700)  ; División vertical 1
+  (send dc draw-line 600 280 600 700) ; División vertical 2
+  )
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;graphics: canvas;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;function to refresh the canvas
 (define (paint-callback canvas dc)
-  (+ 1 1)
+  (send dc set-background "green")
+  (send dc clear)
+  (draw-lines canvas)
  )
 
 ;canvas definition
-(define canvas (new canvas% [parent mainPanel]
+(define canvas (new canvas% [parent player2Panel]
                     [style (list 'border)]
                     [paint-callback paint-callback]
                     ))
